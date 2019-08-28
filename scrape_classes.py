f9a09166ec9d7179e30582f92f14b129aa62cd6d
Danilo Radovic <dradovic505@gmail.com>
2019-08-28 13:51:35 -0700

    first page clicked through

--- scrape_classes.py
+++ scrape_classes.py
@@ -5,18 +5,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Firefox()
-# browser.get('https://pisa.ucsc.edu/class_search/index.php')
-browser.get('http://codepad.org/')
+browser.get('https://pisa.ucsc.edu/class_search/index.php')
 #Make sure we're on the correct site
-# assert 'UC Santa Cruz - Schedule of Classes' in browser.title
+assert 'UC Santa Cruz - Schedule of Classes' in browser.title
+
+# elem = browser.find_element_by_name('select')
+# elem.send_keys('seleniumhq' + Keys.RETURN)
+
+#Keep term at Fall 2019 for now
+term_dropdown = browser.find_element_by_id('term_dropdown')
+#For status, click on all classes instead of open classes
+all_classes = browser.find_element_by_xpath('/html/body/div/form/div/div[2]/div[4]/div/select/option[2]')
+all_classes.click()
+
+search = browser.find_element_by_xpath('/html/body/div/form/div/div[2]/div[15]/div/input')
+search.click()
 
 # html = browser.page_source
 # print(html)
 
-button = browser.find_element_by_xpath('/html/body/div[1]/center/table/tbody/tr/td[1]/div/form/table/tbody/tr[2]/td[1]/nobr[10]/label/input')
-button.click()
+#Now we have all classes listed. Click "Display first" to show 100 results.
+    #We need to click into every class, grab the information needed, store it in
+    #the json file, then click back to results. Then continue to the next class
+    #until at the end of the page then click "next"
 
-# elem = browser.find_element_by_name('select')
-# elem.send_keys('seleniumhq' + Keys.RETURN)
 
 # browser.quit()
